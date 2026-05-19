fbd22e8ad1011ad71dbb6dde9109e4f90ca685b5
Willian Henrique Giroto Domingos <willianhenriquegirotodomingos@MacBook-Air-de-Willian.local>
2026-05-18 22:25:43 -0300

    fix css type declaration

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
